ac1ce2767e0d128749245034fb5d8cb9dfa36e4b
Teooo06 <matteobertoldini06@gmail.com>
2025-03-20 09:03:13 +0100

    Initial commit

---
+++ src/main/java/com/eryxis/eryxis/EryxisApplication.java
@@ -0,0 +1,13 @@
+package com.eryxis.eryxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EryxisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EryxisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eryxis/eryxis/EryxisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eryxis.eryxis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EryxisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
