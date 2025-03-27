ff89d07349cc4eb423e0b955e054dfe6c697443c
Teooo06 <matteobertoldini06@gmail.com>
2025-03-27 10:45:47 +0100

    Modifiche alla mail

--- src/main/java/com/eryxis/eryxis/service/OTPService.java
+++ src/main/java/com/eryxis/eryxis/service/OTPService.java
@@ -1,6 +1,9 @@
 package com.eryxis.eryxis.service;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -24,11 +27,33 @@ public class OTPService {
     }
 
     private void sendOTPEmail(String email, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject("Il tuo codice OTP");
-        message.setText("Il tuo codice OTP è: " + otp);
-        mailSender.send(message);
+        String htmlContent = "<html><body>"
+                + "<p>Caro client,</p>"
+                + "<p>Il tuo codice OTP è: " + otp + "</p>"
+                + "<br><br>"
+                + "<p>--</p>"
+                + "<p>Se la mail non la riguarda ignori il messaggio</p>"
+                + "<p>--</p>"
+                + "<p>Eryxis Bank</p>"
+                + "<p>Soluzioni bancarie sicure e moderne</p>"
+                + "<p>info.eryxis@gmail.com</p>"
+                + "<p>Orari di supporto: Lun-Ven 9:00 - 9:01</p>"
+                + "</body></html>";
+        // Crea l'oggetto MimeMessage
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        try {
+            // Crea un MimeMessageHelper per impostare correttamente l'email
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+            messageHelper.setTo(email);
+            messageHelper.setSubject("Il tuo codice OTP");
+            messageHelper.setText(htmlContent, true); // true indica che il contenuto è HTML
+
+            // Invia l'email
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace(); // Gestisci eventuali errori
+        }
     }
 
     public boolean validateOTP(String email, String otp) {
