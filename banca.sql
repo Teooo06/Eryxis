f90c0450e860f255a5261603e87bfe4b5ce6e412
LupoAlberto69 <moltenilorenzo6@gmail.com>
2025-04-13 14:57:44 +0200

    DB integrity constraints

--- banca.sql
+++ banca.sql
@@ -12,9 +12,9 @@ USE `banca`;
 
 -- Create permessi table first since other tables reference it
 CREATE TABLE `permessi`(
-  `idPermesso` INT PRIMARY KEY,
-  `ruolo` VARCHAR(10) NOT NULL,
-  `codicePermesso` CHAR(2) UNIQUE NOT NULL
+    `idPermesso` INT PRIMARY KEY,
+    `ruolo` VARCHAR(10) NOT NULL,
+    `codicePermesso` CHAR(2) UNIQUE NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `permessi` (`idPermesso`, `ruolo`, `codicePermesso`) VALUES
@@ -28,7 +28,7 @@ CREATE TABLE `utenti`(
     `idUtente` INT PRIMARY KEY AUTO_INCREMENT,
     `nome` VARCHAR(50) NOT NULL,
     `cognome` VARCHAR(50) NOT NULL,
-    `dataNascita` DATE DEFAULT CURRENT_DATE CHECK ( dataNascita BETWEEN '1900-01-01' AND CURRENT_DATE ),
+    `dataNascita` DATE DEFAULT '2000-01-01',
     `indirizzo` VARCHAR(100) NOT NULL,
     `codiceFiscale` VARCHAR(16) UNIQUE NOT NULL,
     `mail` VARCHAR(150) UNIQUE NOT NULL,
@@ -76,7 +76,7 @@ CREATE TABLE `rubriche`(
 
 CREATE TABLE `logs`(
     `idLog` INT PRIMARY KEY,
-    `dataModifica` TIMESTAMP DEFAULT CURRENT_TIMESTAMP CHECK ( dataModifica >= conti.dataApertura ),
+    `dataModifica` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `tipoModifica` VARCHAR(50) NOT NULL,
     `descrizione` VARCHAR(255) NOT NULL,
     `id_utente` INT NOT NULL,
@@ -96,7 +96,7 @@ CREATE TABLE `transazioni`(
 CREATE TABLE `carte`(
     `numeroCarta` VARCHAR(16) PRIMARY KEY,
     `CVV` VARCHAR(4) UNIQUE NOT NULL,
-    `dataScadenza` DATE NOT NULL CHECK ( dataScadenza >= CURRENT_DATE ),
+    `dataScadenza` DATE NOT NULL,
     `PIN` CHAR(5) UNIQUE NOT NULL,
     `tipo` VARCHAR(9) NOT NULL,
     `saldoDisponibile` DECIMAL(20, 2) DEFAULT 0 CHECK ( saldoDisponibile >= 0 ),
@@ -133,4 +133,59 @@ CREATE TABLE `finanziamenti`(
     `importoPagato` DECIMAL(15, 2) DEFAULT 0,
     `id_utente` INT NOT NULL,
     CONSTRAINT `fk_finanziamento_utente` FOREIGN KEY (`id_utente`) REFERENCES `utenti`(`idUtente`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=LATIN1;
+
+-- Checks to verify compliance with integrity constraints
+-- Check if the date of birth is between the current date and the 01/01/1900, if is not in this range it will generate an error
+DELIMITER //
+
+CREATE TRIGGER check_dataNascita BEFORE INSERT ON utenti
+ FOR EACH ROW
+BEGIN
+ IF NEW.dataNascita < '1900-01-01' OR NEW.dataNascita > CURDATE() THEN
+ SIGNAL SQLSTATE '45000'
+ SET MESSAGE_TEXT = 'dataNascita fuori dal range consentito';
+END IF;
+END;
+//
+
+DELIMITER ;
+
+-- Check if the date of the modification is greater than the account opening date, if not it will generate an error
+DELIMITER //
+
+CREATE TRIGGER check_dataModifica BEFORE INSERT ON logs
+ FOR EACH ROW
+BEGIN
+ DECLARE dataAperturaConto DATE;
+
+ SELECT c.dataApertura
+ INTO dataAperturaConto
+ FROM conti c
+JOIN utenti u ON u.idUtente = NEW.id_utente
+ WHERE u.idUtente = NEW.id_utente
+LIMIT 1;
+
+ IF NEW.dataModifica < dataAperturaConto THEN
+ SIGNAL SQLSTATE '45000'
+ SET MESSAGE_TEXT = 'dataModifica non può essere precedente a dataApertura del conto';
+END IF;
+END;
+//
+
+DELIMITER ;
+
+-- Check if the expiry date of the card have passed the current date, if so it will generate an error
+DELIMITER //
+
+CREATE TRIGGER check_dataScadenza BEFORE INSERT ON carte
+ FOR EACH ROW
+BEGIN
+ IF NEW.dataScadenza <= CURDATE() THEN
+ SIGNAL SQLSTATE '45000'
+ SET MESSAGE_TEXT = 'dataNascita fuori dal range consentito';
+END IF;
+END;
+//
+
+DELIMITER ;
