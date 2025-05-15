8c5a883220fcd4fe2c81f7aff27e6cba89f78791
mnico06 <molteni.nico06@gmail.com>
2025-05-15 11:55:56 +0200

    aggiunte funzioni per prendere percentuale investimento specifico e percentuale investimenti complessivi

--- src/main/java/com/eryxis/eryxis/repository/ValoriAzioniRepository.java
+++ src/main/java/com/eryxis/eryxis/repository/ValoriAzioniRepository.java
@@ -5,6 +5,12 @@ import com.eryxis.eryxis.model.ValoriAzioniId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface ValoriAzioniRepository extends JpaRepository<ValoriAzioni, ValoriAzioniId> {
+
+    ValoriAzioni findByIdInvestimentoAndDataValore(int idInvestimento, LocalDate dataValore);
+
 }
