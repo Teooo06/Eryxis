225aacf9f40a6501fa428f7bdb2cb18339fc6aa0
mnico06 <molteni.nico06@gmail.com>
2025-05-24 23:43:40 +0200

    teoricamente finiti gli endpoint per i ticket

--- src/main/java/com/eryxis/eryxis/controller/SupportController.java
+++ src/main/java/com/eryxis/eryxis/controller/SupportController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,6 +26,7 @@ public class SupportController {
     @Autowired
     private UtentiService utentiService;
 
+    // Endpoint per la pagina di supporto
     @GetMapping("/support")
     public String support(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -49,34 +51,7 @@ public class SupportController {
         return "redirect:/login";
     }
 
-    @PostMapping("/support")
-    public String createTicket(Model model,
-                               @RequestParam String titolo,
-                               @RequestParam String descrizione) {
-
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        // Check if the user is authenticated with CustomAuthenticationToken
-        if (auth instanceof CustomAuthenticationToken customAuth) {
-            int userId = customAuth.getIdUtente();
-
-            Utenti utente = utentiService.findByIdUtente(userId);
-            Tickets ticket = new Tickets();
-            ticket.setTitolo(titolo);
-            ticket.setDescrizione(descrizione);
-            ticket.setStato((short) 0);
-            ticket.setUtente(utente);
-            ticketsService.save(ticket);
-
-            List<Tickets> tickets = ticketsService.findByUtente(utente);
-
-            model.addAttribute("tickets", tickets);
-
-            return "support";
-        }
-
-        return "redirect:/login";
-    }
-
+    // Endpoint per la pagina di amministrazione del supporto
     @GetMapping("/supportAdmin")
     public String getAllTicket(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -97,6 +72,85 @@ public class SupportController {
 
             // pagina admin per tickets
             // return "supportAdmin";
+        }
+
+        return "redirect:/login"; // Redirect if the user is not authenticated
+    }
+
+    // Endpoint per la creazione di un ticket
+    @PostMapping("/creaTicket")
+    public String createTicket(Model model,
+                               @RequestParam String titolo,
+                               @RequestParam String descrizione) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        // Check if the user is authenticated with CustomAuthenticationToken
+        if (auth instanceof CustomAuthenticationToken customAuth) {
+            int userId = customAuth.getIdUtente();
+
+            Utenti utente = utentiService.findByIdUtente(userId);
+            Tickets ticket = new Tickets();
+            ticket.setTitolo(titolo);
+            ticket.setDescrizione(descrizione);
+            ticket.setStato((short) 1); // Stato 1 indica che il ticket è in attesa
+            ticket.setUtente(utente);
+            ticketsService.save(ticket);
+
+            List<Tickets> tickets = ticketsService.findByUtente(utente);
+
+            model.addAttribute("tickets", tickets);
+
+            return "support";
+        }
+
+        return "redirect:/login";
+    }
+
+    // Endpoint per mettere in stato di risoluzione un ticket (solo admin)
+    @PatchMapping("/risolviTicket")
+    public String risolviTicket(@RequestParam int idTicket) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        // Check if the user is authenticated with CustomAuthenticationToken
+        if (auth instanceof CustomAuthenticationToken customAuth) {
+            int userId = customAuth.getIdUtente();
+            Utenti utente = utentiService.findByIdUtente(userId);
+            String codicePermesso = utente.getPermesso().getCodicePermesso();
+
+            if (!"07".equals(codicePermesso) && !"05".equals(codicePermesso)) {
+                return "redirect:/login"; // Redirect if the user is not authorized
+            }
+
+            Tickets ticket = ticketsService.findByIdTicket(idTicket);
+            ticket.setStato((short) 2); // Stato 1 indica che il ticket è in stato di risoluzione
+            ticketsService.save(ticket);
+
+            return "redirect:/supportAdmin"; // Redirect to the admin support page
+        }
+
+        return "redirect:/login"; // Redirect if the user is not authenticated
+    }
+
+    // Endpoint per chiudere un ticket (solo admin)
+    @PatchMapping("/chiudiTicket")
+    public String chiudiTicket(@RequestParam int idTicket) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        // Check if the user is authenticated with CustomAuthenticationToken
+        if (auth instanceof CustomAuthenticationToken customAuth) {
+            int userId = customAuth.getIdUtente();
+            Utenti utente = utentiService.findByIdUtente(userId);
+            String codicePermesso = utente.getPermesso().getCodicePermesso();
+
+            if (!"07".equals(codicePermesso) && !"05".equals(codicePermesso)) {
+                return "redirect:/login"; // Redirect if the user is not authorized
+            }
+
+            Tickets ticket = ticketsService.findByIdTicket(idTicket);
+            ticket.setStato((short) 3); // Stato 3 indica che il ticket è chiuso
+            ticketsService.save(ticket);
+
+            return "redirect:/supportAdmin"; // Redirect to the admin support page
         }
 
         return "redirect:/login"; // Redirect if the user is not authenticated
